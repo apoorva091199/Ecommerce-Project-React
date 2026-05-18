dc87098091cdddfa87b77afdad012550e13f2cd0
Apoorva Bhat <bhatapoorva897@gmail.com>
2026-05-18 20:21:03 +0530

    Replacd a tag with link tag in checkout and tracking

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
 import "./TrackingPage.css";
+import { Link } from "react-router-dom";
 export function TrackingPage() {
   return (
     <>
@@ -7,9 +8,9 @@ export function TrackingPage() {
 
       <div className="tracking-page">
         <div className="order-tracking">
-          <a className="back-to-orders-link link-primary" href="/orders">
+          <Link to="/orders" className="back-to-orders-link link-primary">
             View all orders
-          </a>
+          </Link>
 
           <div className="delivery-date">Arriving on Monday, June 13</div>
 
