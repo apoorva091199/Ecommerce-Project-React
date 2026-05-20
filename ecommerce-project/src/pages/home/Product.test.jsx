757e420c5b309572a22d466b1888b2acd450a859
Apoorva Bhat <bhatapoorva897@gmail.com>
2026-05-20 12:21:06 +0530

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,14 +1,17 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Product } from "./Product";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 
-vi.mock('axios')
+vi.mock("axios");
 
 describe("Product Component", () => {
-  it("displays the product details correctly", () => {
-    const product = {
+  let product;
+  let loadCart = vi.fn();
+
+  beforeEach(() => {
+    product = {
       id: "54e0eccd-8f36-462b-b68a-8182611d9add",
       image: "images/products/2-slot-toaster-white.jpg",
       name: "2 Slot Toaster - White",
@@ -19,7 +22,10 @@ describe("Product Component", () => {
       priceCents: 1899,
       keywords: ["toaster", "kitchen", "appliances"],
     };
-    const loadCart = vi.fn();
+    loadCart = vi.fn();
+  });
+
+  it("displays the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
 
     expect(screen.getByText("2 Slot Toaster - White")).toBeInTheDocument();
@@ -39,30 +45,17 @@ describe("Product Component", () => {
     expect(screen.getByText("2197")).toBeInTheDocument();
   });
 
-  it("adds a product to the cart", async() => {
-    const product = {
-      id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-      image: "images/products/2-slot-toaster-white.jpg",
-      name: "2 Slot Toaster - White",
-      rating: {
-        stars: 5,
-        count: 2197,
-      },
-      priceCents: 1899,
-      keywords: ["toaster", "kitchen", "appliances"],
-    };
-    const loadCart = vi.fn();
-
+  it("adds a product to the cart", async () => {
     render(<Product product={product} loadCart={loadCart} />);
 
-    const user=userEvent.setup();
-    const addToCartButton=screen.getByTestId('add-to-cart-button')
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
     await user.click(addToCartButton);
 
     expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
-      productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
-      quantity:1
+      productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+      quantity: 1,
     });
     expect(loadCart).toHaveBeenCalled();
-    });
+  });
 });
