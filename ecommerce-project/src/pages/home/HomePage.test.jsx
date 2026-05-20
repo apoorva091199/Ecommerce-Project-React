64f3abaa6fc5a48d34993786d6dec16da1314849
Apoorva Bhat <bhatapoorva897@gmail.com>
2026-05-20 15:41:26 +0530

    Test clicking add to cart button

---
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -0,0 +1,7 @@
+import { it, expect, describe, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { HomePage } from "./HomePage";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+
+describe()
